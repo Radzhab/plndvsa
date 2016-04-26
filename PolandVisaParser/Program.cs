04f0fa689757317ff06c81d9dc32ed8378828a4b
Ihor Hadzera <ihor.hadzera@D2L.com>
2016-04-26 12:10:59 +0300

    -Changed structure

--- PolandVisaParser/Program.cs
+++ PolandVisaParser/Program.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using Newtonsoft.Json;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
 
 namespace PolandVisaParser
 {
@@ -13,7 +11,7 @@ namespace PolandVisaParser
 	{
 		private static void Main(string[] args)
 		{
-			if (args == null || (args.Length == 0 && !File.Exists(args[0]) ) )
+			if (args == null || (args.Length == 0 || !File.Exists(args[0]) ) )
 			{
 				Console.WriteLine("File name is absent or incorrect! Please check corectness of file name/path and try again");
 				Console.WriteLine("To continue, press any key...");
@@ -27,47 +25,26 @@ namespace PolandVisaParser
 				//webDriver = new ChromeDriver();
 				string inputParameters = File.ReadAllText(args[0]);
 				InputData inputData = JsonConvert.DeserializeObject<InputData>( inputParameters );
+				Scenario scenario = new Scenario();
 				foreach (string city in inputData.Cities)
 				{
-					ChromeOptions chromeOptions = new ChromeOptions();
+					ChromeOptionsWithPrefs chromeOptions = new ChromeOptionsWithPrefs();
 					chromeOptions.AddArgument( "--lang=en" );
 					chromeOptions.AddArgument( "--start-maximized" );
-					
+					chromeOptions.prefs = new Dictionary<string, object>
+					{
+						{ "intl.accept_languages", "en-US,en" }
+					};
+
 					IWebDriver webDriver = new ChromeDriver( chromeOptions );
 					webDriversList.Add(webDriver);
 					
 					//Initial goto url
 					webDriver.Navigate().GoToUrl( inputData.ConsulatUrl );
 
-					//Finding frame with links (screen 1)
-					webDriver.SwitchTo().Frame(
-						webDriver.FindElement(
-							By.CssSelector("iframe[src^=\"https://polandonline.vfsglobal.com/poland-ukraine-appointment\"]")
-						)
-					);
-					//pushing on find date link (screen 1)
-					webDriver.FindElement(By.Id("ctl00_plhMain_lnkSchApp")).Click();
-					
-					//selecting office
-					SelectElement selectOffice = new SelectElement(webDriver.FindElement(By.Id("ctl00_plhMain_cboVAC")));
-					string officeOptionText = selectOffice.Options.First(x => x.Text.Contains(city)).Text;
-					selectOffice.SelectByText( officeOptionText );
-					//selecting office
-					SelectElement selectType = new SelectElement( webDriver.FindElement( By.Id( "ctl00_plhMain_cboPurpose" ) ) );
-					selectType.SelectByValue( "1" );
-					//submiting form
-					webDriver.FindElement( By.Id( "ctl00_plhMain_btnSubmit" ) )
-						.Click();
+					//Run scenario
+					scenario.RunScenario( webDriver, inputData, city );
 
-					//writting count of aplicants
-					webDriver.FindElement( By.Id( "ctl00_plhMain_tbxNumOfApplicants" ) ).Clear();
-					webDriver.FindElement( By.Id( "ctl00_plhMain_tbxNumOfApplicants" ) ).SendKeys( inputData.PeopleCount.ToString());
-						
-					//selecting visa type
-					SelectElement selectVisaType = new SelectElement( webDriver.FindElement( By.Id( "ctl00_plhMain_cboVisaCategory" ) ) );
-					string visaTypeText = selectVisaType.Options.First( x => x.Text.Contains( inputData.VisaType ) ).Text;
-					selectVisaType.SelectByText( visaTypeText );
-					
 					Console.WriteLine( "Parser running. For escape, press any button..." );
 					Console.ReadKey();
 				}
